97f3fa03104e6bc29767ae849749199dccfae8e1
Travis <travis@travis-ci.org>
2016-03-02 15:44:25 +0800

    gg

--- app/src/main/java/com/example/vbfc_bry07/calls/Activity/DoctorsInfoActivity.java
+++ app/src/main/java/com/example/vbfc_bry07/calls/Activity/DoctorsInfoActivity.java
@@ -4,8 +4,9 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.widget.SearchView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,6 +14,7 @@ import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.vbfc_bry07.calls.Adapter.DoctorListAdapter;
 import com.example.vbfc_bry07.calls.Controller.DbHelper;
@@ -23,7 +25,7 @@ import com.example.vbfc_bry07.calls.R;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class DoctorsInfoActivity extends AppCompatActivity implements View.OnClickListener, SearchView.OnQueryTextListener {
+public class DoctorsInfoActivity extends AppCompatActivity implements TextWatcher {
 
     ListView DoctorsListView;
     DbHelper dbHelper;
@@ -31,10 +33,13 @@ public class DoctorsInfoActivity extends AppCompatActivity implements View.OnCli
 
     ArrayList<HashMap<String, String>> all_doctors;
     ListAdapter doctorAdapter;
+    // HashMap<Integer, ArrayList<HashMap<String, String>>> listDataChild;
+    ArrayList<HashMap<String, String>> doctors_array = new ArrayList<>();
+    ArrayList<HashMap<String, Object>> searchResults;
 
-    TextView TxtDoctorName, TxtDoctorSpecialty, TxtDoctorNumber, TxtDoctorClass, TxtDoctorBirthDate;
+    TextView TxtDoctorName, TxtDoctorSpecialty, TxtDoctorNumber, TxtDoctorClass, TxtDoctorBirthDate, no_records;
     String DoctorName, DoctorSpecialty, DoctorNumber, DoctorClass, DoctorBirthDate;
-    SearchView BtnSearch;
+    EditText BtnSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,10 +54,12 @@ public class DoctorsInfoActivity extends AppCompatActivity implements View.OnCli
         TxtDoctorNumber = (TextView) findViewById(R.id.TxtDoctorNumber);
         TxtDoctorClass = (TextView) findViewById(R.id.TxtDoctorClass);
         TxtDoctorBirthDate = (TextView) findViewById(R.id.TxtDoctorBirthDate);
-        BtnSearch = (SearchView) findViewById(R.id.BtnSearch);
+        BtnSearch = (EditText) findViewById(R.id.BtnSearch);
+        no_records = (TextView) findViewById(R.id.no_records);
 
         DoctorsListView = (ListView) findViewById(R.id.doctorsList);
         all_doctors = DC.SelectAllDoctors();
+        doctors_array.addAll(all_doctors);
         doctorAdapter = new DoctorListAdapter(this, R.layout.adapter_doctors_list, all_doctors);
         DoctorsListView.setAdapter(doctorAdapter);
 
@@ -64,7 +71,6 @@ public class DoctorsInfoActivity extends AppCompatActivity implements View.OnCli
         DoctorsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // String selectedFromList = (DoctorsListView.getItemAtPosition(position).toString());
                 DoctorName = all_doctors.get(position).get(DoctorsController.DOCTORS_DOC_NAME);
                 DoctorSpecialty = all_doctors.get(position).get(SpecializationsController.SPECIALIZATION_NAME);
                 DoctorNumber = all_doctors.get(position).get(DoctorsController.DOCTORS_CONTACT_NUMBER);
@@ -76,7 +82,7 @@ public class DoctorsInfoActivity extends AppCompatActivity implements View.OnCli
                 } else {
                     TxtDoctorNumber.setText(DoctorNumber);
                 }
-                if (DoctorNumber.equals("")) {
+                if (DoctorBirthDate.equals("")) {
                     TxtDoctorBirthDate.setText("No Birthdate to display");
                 } else {
                     TxtDoctorBirthDate.setText(DoctorBirthDate);
@@ -84,13 +90,13 @@ public class DoctorsInfoActivity extends AppCompatActivity implements View.OnCli
             }
         });
 
-        BtnSearch.setOnClickListener(this);
-        BtnSearch.setOnQueryTextListener(this);
+        BtnSearch.addTextChangedListener(this);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.search_help, menu);
+        getMenuInflater().inflate(R.menu.help, menu);
         return true;
     }
 
@@ -100,27 +106,53 @@ public class DoctorsInfoActivity extends AppCompatActivity implements View.OnCli
             case android.R.id.home:
                 this.finish();
                 break;
+            case R.id.BtnHelp:
+                Toast.makeText(this, "HELP!", Toast.LENGTH_SHORT).show();
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.BtnSearch:
-                BtnSearch.onActionViewExpanded();
-                break;
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        String search = String.valueOf(s);
+        if(!search.equals("")) {
+            all_doctors.clear();
+
+            for (int x = 0; x < doctors_array.size(); x++) {
+                HashMap<String, String> hash = doctors_array.get(x);
+
+                if (hash.get(DoctorsController.DOCTORS_DOC_NAME).toLowerCase().contains(search.toLowerCase())) {
+                    all_doctors.add(hash);
+                }
+            }
+
+            if (all_doctors.size() > 0) {
+                no_records.setVisibility(View.GONE);
+                DoctorsListView.setVisibility(View.VISIBLE);
+
+                doctorAdapter = new DoctorListAdapter(this, R.layout.adapter_doctors_list, all_doctors);
+                DoctorsListView.setAdapter(doctorAdapter);
+            } else {
+                no_records.setVisibility(View.VISIBLE);
+                DoctorsListView.setVisibility(View.GONE);
+            }
+
+        } else {
+            all_doctors = DC.SelectAllDoctors();
+            doctorAdapter = new DoctorListAdapter(this, R.layout.adapter_doctors_list, all_doctors);
+            DoctorsListView.setAdapter(doctorAdapter);
         }
-    }
 
-    @Override
-    public boolean onQueryTextSubmit(String query) {
-        return false;
-    }
-
-    @Override
-    public boolean onQueryTextChange(String query) {
-        doctorAdapter.setFilterText(query);
-        return false;
     }
 }
